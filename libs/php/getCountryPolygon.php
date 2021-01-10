09faa4e9277c564eb4af50dba57e1dc194ac165d
Romancevic <Romancevic2@gmail.com>
2021-01-10 18:11:15 +0000

    Add files via upload

--- libs/php/getCountryPolygon.php
+++ libs/php/getCountryPolygon.php
@@ -1,17 +1,54 @@
 <?php
-//echo '<pre>';print_r($_POST);
 
-$countryBorders = json_decode(file_get_contents('countryBorders.geo.json'), true);
-$border = null;
+    // ini_set('display_errors', 'On');
+    // error_reporting(E_ALL);
 
-foreach($countryBorders['features'] as $feature) {
+    $executionStartTime = microtime(true);
 
-	if($feature['properties']['ISO_A3'] == $_POST['iso3']) {
-		$border = $feature; break;
-	}
-}
+    // get country border feature
 
-exit(json_encode($border));
+    $countryBorders = json_decode(file_get_contents("countryBorders.geo.json"), true);
+
+    $border = null;
+
+    foreach ($countryBorders['features'] as $feature) {
+
+        if ($feature["properties"]["iso_a2"] ==  $_REQUEST['countryCode']) {
+
+            $border = $feature;
+            break;
+
+        }
+
+    }
+
+
+    $url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=Romancevic&style=full';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    $result=curl_exec($ch);
+
+    curl_close($ch);
+
+    $countryInfo = json_decode($result,true);
+
+
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+    $output['data']['border'] = $border;
+    $output['data']['countryInfo'] = $countryInfo;
+
+    header('Content-Type: application/json; charset=UTF-8');
+
+    echo json_encode($output);
+
+?>
 /* OLD VERSION
 if(isset($_POST['iso3'])) {
 
